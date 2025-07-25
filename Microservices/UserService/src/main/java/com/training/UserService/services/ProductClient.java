7e370a6a3f9d87503b7ab351d96881f10d9f8200
Erudite-tanisha <tanishaconsultadd@gmail.com>
2025-07-25 08:22:44 -0400

    session 8 and 9

--- Microservices/UserService/src/main/java/com/training/UserService/services/ProductClient.java
+++ Microservices/UserService/src/main/java/com/training/UserService/services/ProductClient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-@FeignClient(url= "localhost:8082", value = "Product-Client")
+//@FeignClient(url= "localhost:8082", value = "Product-Client")
+@FeignClient(name = "PRODUCTSERVICE")
 public interface ProductClient {
 
     @GetMapping("/product/user/{userId}")
